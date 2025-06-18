315993c4c1238fffe215abddae717527a2cb16ba
Sem Star <s1150030@student.hsleiden.nl>
2025-06-18 12:59:43 +0200

    types in PHP with var dump

---
+++ PHPcourse/types.php
@@ -0,0 +1,11 @@
+<?php
+
+$isStudent = 1;
+var_dump($isStudent, true, $isStudent == true);
+
+$scores = [85, "90"];
+echo $scores[0] + $scores[1] . "\n";
+var_dump($scores);
+
+
+//Var dunp laat zien welk datatype gebruikt is.
