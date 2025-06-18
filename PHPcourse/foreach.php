72d83c4442ac169f6fcec8ac87d9144c376ecb70
Sem Star <s1150030@student.hsleiden.nl>
2025-06-18 14:56:30 +0200

    foreach loop

---
+++ PHPcourse/foreach.php
@@ -0,0 +1,13 @@
+<?php
+$basket = [
+    "Apple" => 3,
+    "Banana" => 2,
+];
+
+$total = 0;
+
+foreach($basket as $item => $quantity){
+    echo $item.": ".$quantity . "\n";
+    $total += $quantity;
+}
+echo "Total: $total";
