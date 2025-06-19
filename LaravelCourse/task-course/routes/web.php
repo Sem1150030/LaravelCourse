a34e2023480c747bfe1bb0c027a0f82cb2cb5b7f
Sem Star <s1150030@student.hsleiden.nl>
2025-06-19 22:12:39 +0200

    Laravel Routing

--- LaravelCourse/task-course/routes/web.php
+++ LaravelCourse/task-course/routes/web.php
@@ -3,5 +3,25 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return 'Main Page';
+});
+
+//Add a name to the route
+Route::get('/hello', function () {
+   return 'Hello World';
+})->name('hello');;
+
+//URL parameters
+Route::get('/greet/{name}', function ($name) {
+    return 'Hello ' . $name . '!';
+});
+
+//Redirect
+Route::get('/hallo', function () {
+   return redirect()->route('hello');
+});
+
+//
+Route::fallback(function () {
+   return '404 Not Found';
 });
