581c3703f30e297916fe5558c7ced9f97e451218
Sem Star <s1150030@student.hsleiden.nl>
2025-06-27 00:15:18 +0200

    Route model Binding, Form requests, Mass assigning

--- LaravelCourse/task-course/routes/web.php
+++ LaravelCourse/task-course/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Requests\TaskRequest;
 use App\Models\Task;
 use http\Client\Response;
 use Illuminate\Http\Request;
@@ -17,58 +18,30 @@ Route::get('/tasks', function ()  {
 
 Route::view('/tasks/create', 'create')->name('tasks.create');
 
-Route::put('/tasks/{id}', function ($id, Request $request) {
-    $data = $request->validate([
-        'title' => 'required|max:255',
-        'description' => 'required',
-        'long_description' => 'required',
-    ]);
+Route::put('/tasks/{task}', function (Task $task, TaskRequest $request) {
+    $task->update($request->validated());
+    return redirect()->route('tasks.show', ['task' => $task->id])
 
-    $task = Task::findOrFail($id);
-
-    $task->title = $data['title'];
-    $task->description = $data['description'];
-    $task->long_description  = $data['long_description'];
-
-    $task->save();
-
-    return redirect()->route('tasks.show', ['id' => $task->id])
         ->with('success', 'Task updated successfully');
 })->name('tasks.update');
 
-Route::get('/tasks/{id}/edit', function ($id)  {
-    return view('edit', ['task' =>  \App\Models\Task::findOrFail($id)]);
+Route::get('/tasks/{task}/edit', function (Task $task)  {
+    return view('edit',
+        ['task' => $task]
+    );
 })->name('tasks.edit');
 
-
-
-
-Route::get('/tasks/{id}', function ($id)  {
-   return view('show', ['task' =>  \App\Models\Task::findOrFail($id)]);
+Route::get('/tasks/{task}', function (Task $task)  {
+   return view('show', ['task' => $task]);
 })->name('tasks.show');
 
+Route::post('/tasks', function (TaskRequest $request) {
 
-Route::post('/tasks', function (Request $request) {
-    $data = $request->validate([
-       'title' => 'required|max:255',
-       'description' => 'required',
-       'long_description' => 'required',
-    ]);
-
-    $task = new Task;
-
-    $task->title = $data['title'];
-    $task->description = $data['description'];
-    $task->long_description  = $data['long_description'];
-
-    $task->save();
-
+    $task = Task::create($request->validated());
     return redirect()->route('tasks.index')
+
     ->with('success', 'Task created successfully');
 })->name('tasks.store');
-
-
-
 
 Route::fallback(function () {
     return '404 Not Found';
